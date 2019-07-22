8033b763caec4be5cb9f21bda958e02731658da5
Robert Adamczewski <robert.greyroom@gmail.com>
2019-07-22 21:20:34 +0200

    feat: list the fetch response on the website

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,9 +1,10 @@
+const reposURL = document.querySelector('.repos__title--js');
+const reposList = document.querySelector('.repos__list--js');
+
 const url = 'https://api.github.com';
 const user = 'rogreyroom';
 const getRepos = `/users/${user}/repos`;
-
 const fetchURL = `${url}${getRepos}`;
-const myRepos = [];
 
 console.log(fetchURL);
 
@@ -12,6 +13,7 @@ fetch(fetchURL, {method: 'get'})
     return resp.json();
   })
   .then(repos => {
+    const myRepos = [];
     for (const repo of repos) {
       let {name, html_url} = repo;
       myRepos.push({
@@ -19,6 +21,20 @@ fetch(fetchURL, {method: 'get'})
         html_url,
       });
     }
+    return myRepos;
+  })
+  .then(arr => {
+    reposURL.innerHTML = fetchURL;
+    arr.forEach(item => {
+      let {name, url} = item;
+      let listItem = `
+            <li class="repos__item">
+                <a href="${url}" class="repos__link">
+                ${name}
+                </a>
+            </li>`;
+      reposList.insertAdjacentHTML('beforeend', listItem);
+    });
   })
   .catch(err => {
     console.log(err);
